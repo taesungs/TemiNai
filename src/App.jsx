06289b7b20ab636363fffaee551ea09c2a2c3a7c
taesungs <70329288+taesungs@users.noreply.github.com>
2025-11-19 11:31:18 +0900

    [feat] 충돌 사항 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import MenuGrid from "./components/MenuGrid";
 import PromoVideo from "./pages/PromoVideo";
 import MapGuide from "./pages/MapGuide";
@@ -8,17 +8,19 @@ import Quiz from "./pages/Quiz";
 import QuizCategory from "./pages/QuizCategory";
 import QuizResult from "./pages/QuizResult";
 import QuizPlay from "./pages/QuizPlay";
+
 import MemoryGame from "./pages/MemoryGame";
-import MemoryGameintro from "./pages/MemoryGameintro"
+import MemoryGameintro from "./pages/MemoryGameintro";
 import MemoryGamechoice from "./pages/MemoryGamechoice";
 import MemoryGameeasy from "./pages/MemoryGameeasy";
 import MemoryGamehard from "./pages/MemoryGamehard";
+
 import Recommendation from "./pages/Recommendation";
 import AlertSystem from "./pages/AlertSystem";
+
 import MainPage from "./photobooth/MainPage";
 import SelectPage from "./photobooth/SelectPage";
 import PhotoBooth from "./pages/PhotoBooth";
-
 
 function App() {
   return (
@@ -34,17 +36,17 @@ function App() {
           <Route path="/booth" element={<PhotoBooth />} />
 
           <Route path="/chatbot" element={<Chatbot />} />
+
           <Route path="/quiz" element={<Quiz />} />
           <Route path="/QuizCategory" element={<QuizCategory />} />
           <Route path="/quiz/play" element={<QuizPlay />} />
           <Route path="/quiz/Result" element={<QuizResult />} />
 
-
           <Route path="/memory" element={<MemoryGame />} />
           <Route path="/MemoryGameintro" element={<MemoryGameintro />} />
           <Route path="/MemoryGamechoice" element={<MemoryGamechoice />} />
           <Route path="/MemoryGameeasy" element={<MemoryGameeasy />} />
-          <Route path="/MemoryGamehard" element={<MemoryGamehard />} /> 
+          <Route path="/MemoryGamehard" element={<MemoryGamehard />} />
 
           <Route path="/recommend" element={<Recommendation />} />
           <Route path="/alert" element={<AlertSystem />} />
